6fe7bd36af02145aa82d019ad1dc240d0509f4c7
anthony kingston <britdig2@gmail.com>
2025-05-05 13:17:37 +1000

    Change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <header className="header">
           <img src="https://i.imgflip.com/1bij.jpg" />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project">Anthony Kingston</h4>
         </header>
     )
 }
