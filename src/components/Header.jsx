ae5f534be4f48033e9174e337f11df78690330ba
anthony kingston <britdig2@gmail.com>
2025-05-05 13:27:53 +1000

    Change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-                src="/troll-face.png" 
+                src="troll-face.png" 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project">Anthony Kingston</h4>
         </header>
     )
 }
