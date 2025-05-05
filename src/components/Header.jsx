538276f1d94354a113d491b71d7f497ae55de7f2
anthony kingston <britdig2@gmail.com>
2025-05-05 13:18:34 +1000

    Change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,12 @@ import React from "react"
 export default function Header() {
     return (
         <header className="header">
-          <img src="https://i.imgflip.com/1bij.jpg" />
+            <img 
+                src="./src/images/troll-face.png" 
+                className="header--image"
+            />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">Anthony Kingston</h4>
+            <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )
 }
